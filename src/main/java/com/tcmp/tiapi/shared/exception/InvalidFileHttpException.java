287309a1ab9d543a676854bf6a523e7002f3ecf3
David Reyes <david.reyes@tcmpartners.com>
2023-08-04 10:13:19 -0500

    refactor(http-exceptions): inherit from a more specific exception class

--- src/main/java/com/tcmp/tiapi/shared/exception/InvalidFileHttpException.java
+++ src/main/java/com/tcmp/tiapi/shared/exception/InvalidFileHttpException.java
@@ -2,10 +2,11 @@ package com.tcmp.tiapi.shared.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.client.HttpClientErrorException;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class InvalidFileHttpException extends RuntimeException {
-    public InvalidFileHttpException(String message) {
-        super(message);
+public class InvalidFileHttpException extends HttpClientErrorException {
+    public InvalidFileHttpException(String statusText) {
+        super(HttpStatus.BAD_REQUEST, statusText);
     }
 }
--- src/main/java/com/tcmp/tiapi/shared/exception/NotFoundHttpException.java
+++ src/main/java/com/tcmp/tiapi/shared/exception/NotFoundHttpException.java
@@ -2,10 +2,11 @@ package com.tcmp.tiapi.shared.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.client.HttpClientErrorException;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class NotFoundHttpException extends RuntimeException {
-    public NotFoundHttpException(String message) {
-        super(message);
+public class NotFoundHttpException extends HttpClientErrorException {
+    public NotFoundHttpException(String statusText) {
+        super(HttpStatus.NOT_FOUND, statusText);
     }
 }
