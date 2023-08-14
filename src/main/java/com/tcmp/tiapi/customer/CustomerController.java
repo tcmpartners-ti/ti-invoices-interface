8e9cc02e911ceb7e6c3d6dddc648f0434ef31780
David Reyes <david.reyes@tcmpartners.com>
2023-08-14 15:26:07 -0500

    fix(customers): use correct annotation for controller

--- src/main/java/com/tcmp/tiapi/customer/CustomerController.java
+++ src/main/java/com/tcmp/tiapi/customer/CustomerController.java
@@ -10,15 +10,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping("customers")
 @RequiredArgsConstructor
 @Slf4j
