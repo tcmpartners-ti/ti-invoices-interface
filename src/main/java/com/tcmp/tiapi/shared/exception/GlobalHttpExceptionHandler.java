3e5697120e4e53a5246dc798274f86af698a098d
David Reyes <david.reyes@tcmpartners.com>
2023-08-24 14:52:29 -0500

    feat(http-error): add global exception handler

--- src/main/java/com/tcmp/tiapi/shared/exception/GlobalHttpExceptionHandler.java
+++ src/main/java/com/tcmp/tiapi/shared/exception/GlobalHttpExceptionHandler.java
@@ -1,17 +1,41 @@
 package com.tcmp.tiapi.shared.exception;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.Map;
+import java.util.Optional;
 
 @ControllerAdvice
 public class GlobalHttpExceptionHandler {
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Map<String, Object>> handleBodyValidationException(MethodArgumentNotValidException exception) {
+    HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+
+    return ResponseEntity.status(badRequest)
+      .body(Map.of(
+        "statusCode", badRequest.value(),
+        "message", "Could not validate the provided body.",
+        "errors", exception.getFieldErrors().stream().map(error -> Map.of(
+          "field", error.getField(),
+          "error", Optional.ofNullable(error.getDefaultMessage()).orElse("Internal error.")
+        )).toList()
+      ));
+  }
+
   @ExceptionHandler(NotFoundHttpException.class)
   public ResponseEntity<Map<String, Object>> handleNotFoundException(NotFoundHttpException e) {
-    return ResponseEntity.status(e.getStatusCode().value())
-      .body(Map.of("error", e.getMessage()));
+    HttpStatus notFound = HttpStatus.NOT_FOUND;
+
+    return ResponseEntity.status(notFound)
+      .body(Map.of(
+        "statusCode", notFound.value(),
+        "error", e.getMessage()
+      ));
   }
 }
