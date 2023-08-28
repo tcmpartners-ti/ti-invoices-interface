02465a62adf753797fee05ed6c82ef68546f4292
David Reyes <david.reyes@tcmpartners.com>
2023-08-28 11:12:17 -0500

    refactor(invoice): remove ResponseEntity from controller

--- src/main/java/com/tcmp/tiapi/invoice/InvoiceController.java
+++ src/main/java/com/tcmp/tiapi/invoice/InvoiceController.java
@@ -12,7 +12,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -28,33 +27,32 @@ public class InvoiceController {
 
   @GetMapping(path = "{invoiceReference}", produces = MediaType.APPLICATION_JSON_VALUE)
   @Operation(description = "Get an invoice by its reference.")
-  public ResponseEntity<InvoiceDTO> getInvoiceByReference(@PathVariable String invoiceReference) {
+  public InvoiceDTO getInvoiceByReference(@PathVariable String invoiceReference) {
     log.info("Endpoint: /invoices/{}", invoiceReference);
 
     InvoiceMaster invoice = invoiceService.getInvoiceByReference(invoiceReference);
 
-    return ResponseEntity.ok(invoiceMapper.mapEntityToDTO(invoice));
+    return invoiceMapper.mapEntityToDTO(invoice);
   }
 
   @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
   @Operation(description = "Create a single invoice.")
-  public ResponseEntity<InvoiceCreatedDTO> createInvoice(@Valid @RequestBody InvoiceCreationDTO invoiceDTO) {
+  public InvoiceCreatedDTO createInvoice(@Valid @RequestBody InvoiceCreationDTO invoiceDTO) {
     CreateInvoiceEventMessage invoice = invoiceMapper.mapDTOToFTIMessage(invoiceDTO);
-    String createdInvoiceCorrelationId = invoiceService.sendInvoiceAndGetCorrelationId(invoice);
+    invoiceService.sendInvoiceAndGetCorrelationId(invoice);
 
-    return ResponseEntity.ok(InvoiceCreatedDTO.builder()
+    return InvoiceCreatedDTO.builder()
       .message("Invoice sent to be created.")
-      .invoice(new InvoiceCreatedDTO.InvoiceDTO(createdInvoiceCorrelationId))
-      .build());
+      .build();
   }
 
   @PostMapping(path = "bulk", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
   @Operation(description = "Create multiple invoices.")
-  public ResponseEntity<InvoicesCreatedDTO> createInvoicesBulk(@RequestParam MultipartFile invoicesFile) {
+  public InvoicesCreatedDTO createInvoicesBulk(@RequestParam MultipartFile invoicesFile) {
     invoiceService.createMultipleInvoices(invoicesFile);
 
-    return ResponseEntity.ok(InvoicesCreatedDTO.builder()
+    return InvoicesCreatedDTO.builder()
       .message("Invoices sent to be created.")
-      .build());
+      .build();
   }
 }
