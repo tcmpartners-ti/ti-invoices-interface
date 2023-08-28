eb535f8d401f374d9fa45f4ca56ff36ed80aad40
David Reyes <david.reyes@tcmpartners.com>
2023-08-28 11:12:41 -0500

    refactor(invoice): remove invoice from invoice created response

--- src/main/java/com/tcmp/tiapi/invoice/dto/response/InvoiceCreatedDTO.java
+++ src/main/java/com/tcmp/tiapi/invoice/dto/response/InvoiceCreatedDTO.java
@@ -1,7 +1,6 @@
 package com.tcmp.tiapi.invoice.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,14 +9,4 @@ import lombok.Data;
 public class InvoiceCreatedDTO {
   @Schema(name = "message", description = "Operation result message.")
   private String message;
-
-  @Schema(name = "invoice", description = "Basic invoice information.")
-  private InvoiceDTO invoice;
-
-  @Data
-  @AllArgsConstructor
-  public static class InvoiceDTO {
-    @Schema(name = "id", description = "Generated UUID. (TBD)")
-    private String correlationId;
-  }
 }
