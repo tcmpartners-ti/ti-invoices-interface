5cd1037e53f1fd885df00aeeb01a7098dd0d2fbd
David Reyes <david.reyes@tcmpartners.com>
2023-08-31 15:14:03 -0500

    refactor(program): remove unused creation endpoint

--- src/main/java/com/tcmp/tiapi/program/ProgramController.java
+++ src/main/java/com/tcmp/tiapi/program/ProgramController.java
@@ -1,17 +1,15 @@
 package com.tcmp.tiapi.program;
 
-import com.tcmp.tiapi.program.dto.request.ProgramCreationDTO;
-import com.tcmp.tiapi.program.dto.response.ProgramCreatedDTO;
 import com.tcmp.tiapi.program.dto.response.ProgramDTO;
-import com.tcmp.tiapi.program.messaging.SCFProgrammeMessage;
-import com.tcmp.tiapi.program.model.Program;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("programs")
@@ -20,23 +18,10 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "Programs", description = "Defines the programs operations.")
 public class ProgramController {
   private final ProgramService programService;
-  private final ProgramMapper programMapper;
 
   @GetMapping(path = "{programId}", produces = MediaType.APPLICATION_JSON_VALUE)
   @Operation(description = "Get a program by its identifier.")
   public ProgramDTO getProgramById(@PathVariable String programId) {
-    Program program = programService.getProgramById(programId);
-    return programMapper.mapEntityToDTO(program);
-  }
-
-  @PostMapping
-  @Operation(description = "Creates a program in Trade Innovation.")
-  public ProgramCreatedDTO createProgram(@Valid @RequestBody ProgramCreationDTO programCreationDTO) {
-    SCFProgrammeMessage createProgramMessage = programMapper.mapDTOToFTIMessage(programCreationDTO);
-    programService.sendProgramToBeCreated(createProgramMessage);
-
-    return ProgramCreatedDTO.builder()
-      .message("Program sent to be created.")
-      .build();
+    return programService.getProgramById(programId);
   }
 }
