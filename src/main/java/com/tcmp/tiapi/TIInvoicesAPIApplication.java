33c0994d81cda64ffc731c52fcae33f12921eafa
David Reyes <david.reyes@tcmpartners.com>
2023-09-05 11:24:22 -0500

    build(feign): add dependencies and required annotation in main class

--- src/main/java/com/tcmp/tiapi/TIInvoicesAPIApplication.java
+++ src/main/java/com/tcmp/tiapi/TIInvoicesAPIApplication.java
@@ -2,8 +2,10 @@ package com.tcmp.tiapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class TIInvoicesAPIApplication {
   public static void main(String[] args) {
     SpringApplication.run(TIInvoicesAPIApplication.class, args);
