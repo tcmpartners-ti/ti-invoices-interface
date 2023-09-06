afdd217ae3f60a8d647d1a7a7a0882faeb49ade0
David Reyes <david.reyes@tcmpartners.com>
2023-09-06 11:59:59 -0500

    feat(shared): add documentation to PageParams

--- src/main/java/com/tcmp/tiapi/shared/dto/request/PageParams.java
+++ src/main/java/com/tcmp/tiapi/shared/dto/request/PageParams.java
@@ -1,12 +1,25 @@
 package com.tcmp.tiapi.shared.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+
+import static io.swagger.v3.oas.annotations.media.Schema.*;
 
 @Data
 public class PageParams {
   private static final int DEFAULT_PAGE = 0;
   private static final int DEFAULT_SIZE = 10;
 
+  @Schema(
+    description = "Page to request (zero-based counting).",
+    defaultValue = "0",
+    requiredMode = RequiredMode.NOT_REQUIRED
+  )
   private int page = DEFAULT_PAGE;
+  @Schema(
+    description = "Page size.",
+    defaultValue = "10",
+    requiredMode = RequiredMode.NOT_REQUIRED
+  )
   private int size = DEFAULT_SIZE;
 }
