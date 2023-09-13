617f975808453eba85d0dc1d16f7bab1f968f94f
David Reyes <david.reyes@tcmpartners.com>
2023-09-13 09:39:21 -0500

    feat(api-gee): add Exceptions for better error handling in http request & camel route

---
+++ src/main/java/com/tcmp/tiapi/titoapigee/exception/RecoverableApiGeeRequestException.java
@@ -0,0 +1,7 @@
+package com.tcmp.tiapi.titoapigee.exception;
+
+public class RecoverableApiGeeRequestException extends RuntimeException {
+  public RecoverableApiGeeRequestException(String message) {
+    super(message);
+  }
+}
---
+++ src/main/java/com/tcmp/tiapi/titoapigee/exception/UnrecoverableApiGeeRequestException.java
@@ -0,0 +1,7 @@
+package com.tcmp.tiapi.titoapigee.exception;
+
+public class UnrecoverableApiGeeRequestException extends RuntimeException {
+  public UnrecoverableApiGeeRequestException(String message) {
+    super(message);
+  }
+}
