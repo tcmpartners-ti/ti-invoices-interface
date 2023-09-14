66960ee3d82b85c36e16c6b0bc2b863454bf2352
David Reyes <david.reyes@tcmpartners.com>
2023-09-14 10:16:11 -0500

    feat(api-gee): add OperationalGatewayService

--- src/main/java/com/tcmp/tiapi/titoapigee/service/OperationalGatewayService.java
+++ src/main/java/com/tcmp/tiapi/titoapigee/service/OperationalGatewayService.java
@@ -1,14 +1,21 @@
 package com.tcmp.tiapi.titoapigee.service;
 
-import com.tcmp.tiapi.invoice.dto.request.InvoiceCorrelationPayload;
+import com.tcmp.tiapi.invoice.dto.request.InvoiceTiMessagePayload;
 import com.tcmp.tiapi.messaging.model.response.ServiceResponse;
+import com.tcmp.tiapi.titoapigee.client.ApiGeeHeaderSigner;
 import com.tcmp.tiapi.titoapigee.client.OperationalGatewayClient;
 import com.tcmp.tiapi.titoapigee.dto.request.*;
+import com.tcmp.tiapi.titoapigee.exception.RecoverableApiGeeRequestException;
+import com.tcmp.tiapi.titoapigee.exception.UnrecoverableApiGeeRequestException;
 import com.tcmp.tiapi.titoapigee.mapper.OperationalGatewayMapper;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -17,13 +24,14 @@ import java.util.UUID;
 public class OperationalGatewayService {
   private final OperationalGatewayClient operationalGatewayClient;
   private final OperationalGatewayMapper operationalGatewayMapper;
+  private final ApiGeeHeaderSigner apiGeeHeaderSigner;
 
-  public void sendInvoiceCreationResult(ServiceResponse serviceResponse, InvoiceCorrelationPayload invoiceInfo) {
+  public void sendInvoiceCreationResult(ServiceResponse serviceResponse, InvoiceTiMessagePayload invoiceInfo) {
     OperationalGatewayRequestPayload requestPayload = operationalGatewayMapper.mapTiServiceResponseToOperationalGatewayPayload(
       serviceResponse, invoiceInfo);
 
-    OperationalGatewayRequest request = OperationalGatewayRequest.builder()
-      .data(OperationalGatewayRequestData.builder()
+    ApiGeeBaseRequest<OperationalGatewayRequest> requestBody = ApiGeeBaseRequest.<OperationalGatewayRequest>builder()
+      .data(OperationalGatewayRequest.builder()
         .referenceData(ReferenceData.builder()
           .provider("Provider 123")
           .correlatedMessageId(UUID.randomUUID().toString())
@@ -35,8 +43,27 @@ public class OperationalGatewayService {
         .build())
       .build();
 
-      Object response = operationalGatewayClient.sendInvoiceCreationResult(request);
+    Map<String, String> requestHeaders = apiGeeHeaderSigner.buildRequestHeaders(requestBody);
 
-    log.info("Service response: {}", response);
+    try {
+      operationalGatewayClient.sendInvoiceCreationResult(requestHeaders, requestBody);
+      log.info("Invoice creation notified successfully.");
+    } catch (FeignException e) {
+      List<Integer> unrecoverableResponseCodes = List.of(
+        HttpStatus.BAD_REQUEST.value(),
+        HttpStatus.UNAUTHORIZED.value(),
+        HttpStatus.FORBIDDEN.value(),
+        HttpStatus.NOT_FOUND.value()
+      );
+
+      boolean isUnrecoverableRequest = unrecoverableResponseCodes.contains(e.status());
+      if (isUnrecoverableRequest) {
+        log.error("Client error. {}.", e.getMessage());
+        throw new UnrecoverableApiGeeRequestException(e.getMessage());
+      }
+
+      log.error("Server error. {}.", e.getMessage());
+      throw new RecoverableApiGeeRequestException(e.getMessage());
+    }
   }
 }
