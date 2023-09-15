e45a2a53c95fdb3ef9c7fa6241a68e30c8f77288
David Reyes <david.reyes@tcmpartners.com>
2023-09-15 10:38:33 -0500

    refactor(invoice): clean up code and add error handling in InvoiceCreatedEventListenerRouteBuilder

--- src/main/java/com/tcmp/tiapi/invoice/route/InvoiceCreatedEventListenerRouteBuilder.java
+++ src/main/java/com/tcmp/tiapi/invoice/route/InvoiceCreatedEventListenerRouteBuilder.java
@@ -1,17 +1,21 @@
 package com.tcmp.tiapi.invoice.route;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.tcmp.tiapi.invoice.dto.request.InvoiceCorrelationPayload;
 import com.tcmp.tiapi.messaging.model.response.ServiceResponse;
+import com.tcmp.tiapi.titoapigee.exception.RecoverableApiGeeRequestException;
+import com.tcmp.tiapi.titoapigee.exception.UnrecoverableApiGeeRequestException;
 import com.tcmp.tiapi.titoapigee.service.OperationalGatewayService;
 import lombok.RequiredArgsConstructor;
+import org.apache.camel.Exchange;
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.converter.jaxb.JaxbDataFormat;
 
 @RequiredArgsConstructor
 public class InvoiceCreatedEventListenerRouteBuilder extends RouteBuilder {
+  private static final int MAX_RETRY_ATTEMPTS = 3;
+  private static final int RETRIES_DELAY_IN_MS = 1_000;
+
   private final JaxbDataFormat jaxbDataFormat;
-  private final ObjectMapper objectMapper;
   private final OperationalGatewayService operationalGatewayService;
 
   private final String uriFrom;
@@ -19,17 +23,32 @@ public class InvoiceCreatedEventListenerRouteBuilder extends RouteBuilder {
 
   @Override
   public void configure() {
-    from(uriFrom).routeId("invoiceCreationResult")
-      .log("${body}")
-      .unmarshal(jaxbDataFormat)
-      .process(exchange -> {
-        ServiceResponse serviceResponse = exchange.getIn().getBody(ServiceResponse.class);
-        String correlationJsonPayload = (String) exchange.getIn().getHeaders().get("JMSCorrelationID");
-        InvoiceCorrelationPayload invoiceInfo = objectMapper.readValue(correlationJsonPayload, InvoiceCorrelationPayload.class);
+    onException(UnrecoverableApiGeeRequestException.class)
+      .log(LoggingLevel.ERROR, "Could not notify invoice creation.")
+      .end();
 
-        operationalGatewayService.sendInvoiceCreationResult(serviceResponse, invoiceInfo);
-      })
+    onException(RecoverableApiGeeRequestException.class)
+      .maximumRedeliveries(MAX_RETRY_ATTEMPTS)
+      .redeliveryDelay(RETRIES_DELAY_IN_MS)
       .to(uriTo)
       .end();
+
+    from(uriFrom).routeId("invoiceCreationResult")
+      .unmarshal(jaxbDataFormat)
+      .to("log:body")
+      .end();
+
+    from(uriTo).routeId("apiGeeInvoiceCreationNotifier")
+      .process(this::sendInvoicePayloadOrThrowException)
+      .end();
+  }
+
+  private void sendInvoicePayloadOrThrowException(Exchange exchange) {
+    ServiceResponse serviceResponse = exchange.getIn().getBody(ServiceResponse.class);
+    if (serviceResponse == null) return;
+    log.info("ServiceResponse={}", serviceResponse);
+
+    String invoiceNumber = serviceResponse.getResponseHeader().getCorrelationId();
+    operationalGatewayService.sendInvoiceCreationResult(serviceResponse, invoiceNumber);
   }
 }
