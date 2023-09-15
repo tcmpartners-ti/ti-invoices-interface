88e30a3d2fd4de008d978c17fa5fd9e047251ae5
David Reyes <david.reyes@tcmpartners.com>
2023-09-15 10:38:09 -0500

    refactor(invoice): clean up code in CreateInvoiceRouteBuilder

--- src/main/java/com/tcmp/tiapi/invoice/route/CreateInvoiceRouteBuilder.java
+++ src/main/java/com/tcmp/tiapi/invoice/route/CreateInvoiceRouteBuilder.java
@@ -5,6 +5,7 @@ import com.tcmp.tiapi.messaging.TIServiceRequestWrapper;
 import com.tcmp.tiapi.messaging.model.TIOperation;
 import com.tcmp.tiapi.messaging.model.TIService;
 import com.tcmp.tiapi.messaging.model.requests.ReplyFormat;
+import com.tcmp.tiapi.messaging.model.requests.ServiceRequest;
 import com.tcmp.tiapi.messaging.router.processor.XmlNamespaceFixer;
 import lombok.RequiredArgsConstructor;
 import org.apache.camel.builder.RouteBuilder;
@@ -22,16 +23,20 @@ public class CreateInvoiceRouteBuilder extends RouteBuilder {
   @Override
   public void configure() {
     from(uriFrom).routeId("createInvoiceInTI")
-      .transform().body(CreateInvoiceEventMessage.class, createInvoiceEventMessage -> tiServiceRequestWrapper.wrapRequest(
-        TIService.TRADE_INNOVATION,
-        TIOperation.CREATE_INVOICE,
-        ReplyFormat.STATUS,
-        createInvoiceEventMessage.getInvoiceNumber(), // CorrelationId
-        createInvoiceEventMessage
-      ))
+      .transform().body(CreateInvoiceEventMessage.class, this::wrapToServiceRequest)
       .marshal(jaxbDataFormat)
       .transform().body(String.class, xmlNamespaceFixer::fixNamespaces)
       .to(uriTo)
       .end();
+  }
+
+  private ServiceRequest<CreateInvoiceEventMessage> wrapToServiceRequest(CreateInvoiceEventMessage message) {
+    return tiServiceRequestWrapper.wrapRequest(
+      TIService.TRADE_INNOVATION,
+      TIOperation.CREATE_INVOICE,
+      ReplyFormat.STATUS,
+      message.getInvoiceNumber(),
+      message
+    );
   }
 }
