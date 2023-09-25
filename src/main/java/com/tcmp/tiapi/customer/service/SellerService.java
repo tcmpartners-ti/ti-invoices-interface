c4e0eef646d24db590b13ff76f5270cb90f61edd
David Reyes <david.reyes@tcmpartners.com>
2023-09-25 14:47:00 -0500

    refactor(customer): simplify get seller invoices method

--- src/main/java/com/tcmp/tiapi/customer/service/SellerService.java
+++ src/main/java/com/tcmp/tiapi/customer/service/SellerService.java
@@ -1,16 +1,11 @@
 package com.tcmp.tiapi.customer.service;
 
 import com.tcmp.tiapi.customer.dto.response.SearchSellerInvoicesParams;
-import com.tcmp.tiapi.customer.model.CounterParty;
-import com.tcmp.tiapi.customer.model.CounterPartyRole;
-import com.tcmp.tiapi.customer.repository.CounterPartyRepository;
 import com.tcmp.tiapi.invoice.InvoiceMapper;
-import com.tcmp.tiapi.invoice.repository.InvoiceRepository;
 import com.tcmp.tiapi.invoice.dto.response.InvoiceDTO;
 import com.tcmp.tiapi.invoice.model.InvoiceMaster;
+import com.tcmp.tiapi.invoice.repository.InvoiceRepository;
 import com.tcmp.tiapi.invoice.repository.InvoiceSpecifications;
-import com.tcmp.tiapi.program.ProgramRepository;
-import com.tcmp.tiapi.program.model.Program;
 import com.tcmp.tiapi.shared.dto.request.PageParams;
 import com.tcmp.tiapi.shared.dto.response.paginated.PaginatedResult;
 import com.tcmp.tiapi.shared.dto.response.paginated.PaginatedResultMeta;
@@ -19,21 +14,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class SellerService {
-  private final CounterPartyRepository counterPartyRepository;
   private final InvoiceRepository invoiceRepository;
-  private final ProgramRepository programRepository;
-
   private final InvoiceMapper invoiceMapper;
 
   public PaginatedResult<InvoiceDTO> getSellerInvoices(
@@ -41,64 +30,29 @@ public class SellerService {
     SearchSellerInvoicesParams searchParams,
     PageParams pageParams
   ) {
-    List<Long> sellerCounterpartyIds = counterPartyRepository.findUniqueIdsByCustomerMnemonicAndRole(
-      sellerMnemonic, CounterPartyRole.SELLER.getValue());
-
-    if (sellerCounterpartyIds.isEmpty()) {
-      throw new NotFoundHttpException(
-        String.format("Could not find invoices for seller with mnemonic %s.", sellerMnemonic));
-    }
-
-    Specification<InvoiceMaster> searchSpec = InvoiceSpecifications.filterBySellerIdsAndStatus(
-      sellerCounterpartyIds,
-      searchParams.status()
-    );
-    Page<InvoiceMaster> invoiceMasterPage = invoiceRepository.findAll(
-      searchSpec,
+    Page<InvoiceMaster> sellerInvoicesPage = invoiceRepository.findAll(
+      InvoiceSpecifications.filterBySellerMnemonicAndStatus(sellerMnemonic, searchParams.status()),
       PageRequest.of(pageParams.getPage(), pageParams.getSize())
     );
 
-    Map<Long, CounterParty> idsToCounterParties = getIdsToCounterpartiesFromInvoicesPageAndSellerIds(invoiceMasterPage, sellerCounterpartyIds);
-    Map<Long, Program> idsToPrograms = getIdsToProgramsFromInvoicesPage(invoiceMasterPage);
+    if (sellerInvoicesPage.isEmpty()) {
+      throw new NotFoundHttpException(
+        String.format(
+          "Could not find invoices for seller with mnemonic %s and status %s.",
+          sellerMnemonic,
+          searchParams.status()
+        ));
+    }
 
-    List<InvoiceDTO> invoicesDTOs = invoiceMapper.mapEntitiesToDTOs(invoiceMasterPage.getContent(), idsToCounterParties, idsToPrograms);
+    List<InvoiceDTO> invoicesDTOs = invoiceMapper.mapEntitiesToDTOs(sellerInvoicesPage.getContent());
 
     return PaginatedResult.<InvoiceDTO>builder()
       .data(invoicesDTOs)
       .meta(PaginatedResultMeta.builder()
-        .isLastPage(invoiceMasterPage.isLast())
-        .totalPages(invoiceMasterPage.getTotalPages())
-        .totalItems(invoiceMasterPage.getTotalElements())
+        .isLastPage(sellerInvoicesPage.isLast())
+        .totalPages(sellerInvoicesPage.getTotalPages())
+        .totalItems(sellerInvoicesPage.getTotalElements())
         .build())
       .build();
-  }
-
-  private Map<Long, CounterParty> getIdsToCounterpartiesFromInvoicesPageAndSellerIds(
-    Page<InvoiceMaster> invoiceMasterPage,
-    List<Long> sellerCounterpartyIds
-  ) {
-    List<Long> buyerCounterpartyIds = invoiceMasterPage.get()
-      .map(InvoiceMaster::getBuyerId)
-      .distinct()
-      .toList();
-
-    List<Long> uniqueCounterpartiesIds = Stream.concat(sellerCounterpartyIds.stream(), buyerCounterpartyIds.stream())
-      .distinct()
-      .toList();
-
-    return counterPartyRepository.findByIdIn(uniqueCounterpartiesIds)
-      .stream()
-      .collect(Collectors.toMap(CounterParty::getId, counterParty -> counterParty));
-  }
-
-  private Map<Long, Program> getIdsToProgramsFromInvoicesPage(Page<InvoiceMaster> invoiceMasterPage) {
-    List<Long> uniqueProgramIds = invoiceMasterPage.stream()
-      .map(InvoiceMaster::getProgrammeId)
-      .distinct()
-      .toList();
-
-    return programRepository.findByPkIn(uniqueProgramIds)
-      .stream()
-      .collect(Collectors.toMap(Program::getPk, program -> program));
   }
 }
