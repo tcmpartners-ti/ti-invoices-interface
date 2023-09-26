53c321f731cd99e103d27f5c694b669543397b88
David Reyes <david.reyes@tcmpartners.com>
2023-09-26 17:28:59 -0500

    feat(api-gee): add header signers beans in configuration

--- src/main/java/com/tcmp/tiapi/titoapigee/ApiGeeConfiguration.java
+++ src/main/java/com/tcmp/tiapi/titoapigee/ApiGeeConfiguration.java
@@ -1,42 +1,42 @@
-package com.tcmp.tiapi.titoapigee.configuration;
+package com.tcmp.tiapi.titoapigee;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.tcmp.tiapi.titoapigee.client.ApiGeeBodyEncryptionInterceptor;
-import com.tcmp.tiapi.titoapigee.client.ApiGeeHeaderSigner;
+import com.tcmp.tiapi.titoapigee.businessbanking.BusinessBankingHeaderSigner;
+import com.tcmp.tiapi.titoapigee.paymentexecution.PaymentExecutionHeaderSigner;
+import com.tcmp.tiapi.titoapigee.security.HeaderSigner;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.UUID;
-
 @Configuration
 public class ApiGeeConfiguration {
+  private static final String DEVICE_IP = "10.0.0.1";
   @Value("${bp.api-gee.headers.device}") private String device;
   @Value("${bp.api-gee.app-id}") private String appId;
   @Value("${bp.api-gee.api-key}") private String apiEncryptionKey;
   @Value("${bp.api-gee.api-secret}") private String apiSecret;
 
   @Bean
-  public ApiGeeHeaderSigner headerSigner() {
-    String deviceIp = "10.0.0.1";
-    String session = UUID.randomUUID().toString();
-
-    return new ApiGeeHeaderSigner(
+  public HeaderSigner paymentExecutionHeaderSigner() {
+    return new PaymentExecutionHeaderSigner(
       new ObjectMapper(),
-      device,
-      deviceIp,
-      session,
       appId,
       apiEncryptionKey,
-      apiSecret
+      apiSecret,
+      device,
+      DEVICE_IP
     );
   }
 
   @Bean
-  public ApiGeeBodyEncryptionInterceptor operationalGatewaySigner() {
-    return new ApiGeeBodyEncryptionInterceptor(
+  public HeaderSigner businessBankingHeaderSigner() {
+    return new BusinessBankingHeaderSigner(
+      new ObjectMapper(),
+      appId,
       apiEncryptionKey,
-      apiSecret
+      apiSecret,
+      device,
+      DEVICE_IP
     );
   }
 }
