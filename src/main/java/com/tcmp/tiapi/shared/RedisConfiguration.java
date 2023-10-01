aa59c9e837003fa33aad759cd59025acacd21fdd
David Reyes <david.reyes@tcmpartners.com>
2023-10-01 15:45:01 -0500

    fix(shared): remove JpaConfiguration (for correct app start) and move RedisConfiguration

--- src/main/java/com/tcmp/tiapi/shared/RedisConfiguration.java
+++ src/main/java/com/tcmp/tiapi/shared/RedisConfiguration.java
@@ -1,4 +1,4 @@
-package com.tcmp.tiapi.shared.configuration;
+package com.tcmp.tiapi.shared;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
