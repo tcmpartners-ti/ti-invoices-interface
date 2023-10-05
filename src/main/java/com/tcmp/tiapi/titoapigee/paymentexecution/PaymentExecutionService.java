67d49a890899e2fb584859a049ba338cb5f535a4
David Reyes <david.reyes@tcmpartners.com>
2023-10-05 12:32:02 -0500

    refactor(api-gee): inject HeaderSigner in PaymentExecutionService

--- src/main/java/com/tcmp/tiapi/titoapigee/paymentexecution/PaymentExecutionService.java
+++ src/main/java/com/tcmp/tiapi/titoapigee/paymentexecution/PaymentExecutionService.java
@@ -4,25 +4,17 @@ import com.tcmp.tiapi.titoapigee.dto.request.ApiGeeBaseRequest;
 import com.tcmp.tiapi.titoapigee.paymentexecution.dto.request.*;
 import com.tcmp.tiapi.titoapigee.paymentexecution.dto.response.BusinessAccountTransfersResponse;
 import com.tcmp.tiapi.titoapigee.security.HeaderSigner;
-import org.springframework.beans.factory.annotation.Qualifier;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class PaymentExecutionService {
-  private final HeaderSigner apiGeeHeaderSigner;
+  private final HeaderSigner paymentExecutionHeaderSigner;
   private final PaymentExecutionClient paymentExecutionClient;
-
-  public PaymentExecutionService(
-    @Qualifier("paymentExecutionHeaderSigner")
-    HeaderSigner apiGeeHeaderSigner,
-    PaymentExecutionClient paymentExecutionClient
-  ) {
-    this.apiGeeHeaderSigner = apiGeeHeaderSigner;
-    this.paymentExecutionClient = paymentExecutionClient;
-  }
 
   public BusinessAccountTransfersResponse executeClientToBglTransaction(
     String clientAccount,
@@ -43,7 +35,7 @@ public class PaymentExecutionService {
       .build();
     ApiGeeBaseRequest<TransactionRequest> request = new ApiGeeBaseRequest<>(requestData);
 
-    Map<String, String> headers = apiGeeHeaderSigner.buildRequestHeaders(request);
+    Map<String, String> headers = paymentExecutionHeaderSigner.buildRequestHeaders(request);
 
     return paymentExecutionClient.postPayment(
       headers,
