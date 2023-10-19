76fbb27b83c93353bcee7879355777ab761df2c6
David Reyes <david.reyes@tcmpartners.com>
2023-10-19 15:47:14 -0500

    feat(invoice): add annotations for open csv

--- src/main/java/com/tcmp/tiapi/invoice/dto/InvoiceCreationRowCSV.java
+++ src/main/java/com/tcmp/tiapi/invoice/dto/InvoiceCreationRowCSV.java
@@ -1,5 +1,8 @@
 package com.tcmp.tiapi.invoice.dto;
 
+import com.opencsv.bean.CsvBindByPosition;
+import com.tcmp.tiapi.shared.FieldValidationRegex;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -8,8 +11,6 @@ import org.apache.camel.dataformat.bindy.annotation.CsvRecord;
 import org.apache.camel.dataformat.bindy.annotation.DataField;
 
 import java.io.Serializable;
-import java.math.BigDecimal;
-import java.time.LocalDate;
 
 @CsvRecord(separator = ",")
 @AllArgsConstructor
@@ -21,62 +22,100 @@ import java.time.LocalDate;
 public class InvoiceCreationRowCSV implements Serializable {
   private static final String NUMBER_FORMAT = "#.##";
   private static final String DATE_FORMAT = "dd-MM-yyyy";
-  private static final String DATE_FORMAT_REGEX = "^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-(\\d{4})$";
 
-  @Size(min = 1, max = 20, message = "Customer must be between 1 and 20 characters.")
+  @NotNull(message = "This field is required.")
+  @Size(min = 1, max = 20, message = "This field must be between 1 and 20 characters.")
+  @Pattern(regexp = FieldValidationRegex.ONLY_NUMERIC_VALUES, message = "Only numeric values are allowed.")
   @DataField(pos = 1, name = "CustomerMnemonic")
-  private String customerMnemonic;
+  @CsvBindByPosition(position = 0)
+  private String customer;
 
-  @Size(min = 1, max = 34, message = "Their reference must be between 1 and 34 characters.")
+  @NotNull(message = "This field is required.")
+  @Size(min = 1, max = 34, message = "This field must be between 1 and 34 characters.")
   @DataField(pos = 2, name = "TheirReference")
+  @CsvBindByPosition(position = 1)
   private String theirReference;
 
-  @Size(min = 1, max = 8, message = "Behalf of Branch must be between 1 and 8 characters.")
+  @NotNull(message = "This field is required.")
+  @NotBlank(message = "This field is required.")
+  @Size(min = 1, max = 8, message = "This field must be between 1 and 8 characters.")
   @DataField(pos = 3, name = "BehalfOfBranch")
+  @CsvBindByPosition(position = 2)
   private String behalfOfBranch;
 
+  @NotNull(message = "This field is required.")
+  @Size(min = 1, max = 20, message = "This field must be between 1 and 20 characters.")
+  @Pattern(regexp = FieldValidationRegex.ONLY_NUMERIC_VALUES, message = "Only numeric values are allowed.")
   @DataField(pos = 4, name = "AnchorPartyMnemonic")
-  private String anchorPartyMnemonic;
+  @CsvBindByPosition(position = 3)
+  private String anchorParty;
 
-  @NotNull(message = "Programme identifier is required.")
-  @Size(max = 35, message = "Program identifier max length is 35 characters.")
+  @NotNull(message = "This field is required.")
+  @Size(min = 1, max = 35, message = "This field must be between 1 and 35 characters.")
   @DataField(pos = 5, name = "ProgrammeId")
-  private String programmeId;
+  @CsvBindByPosition(position = 4)
+  private String programme;
 
-  @Size(min = 1, max = 20, message = "Seller mnemonic must be between 1 and 20 characters.")
+  @NotNull(message = "This field is required.")
+  @Size(min = 1, max = 20, message = "This field must be between 1 and 20 characters.")
+  @Pattern(regexp = FieldValidationRegex.ONLY_NUMERIC_VALUES, message = "Only numeric values are allowed.")
   @DataField(pos = 6, name = "SellerId")
-  private String sellerId;
+  @CsvBindByPosition(position = 5)
+  private String seller;
 
-  @Size(min = 1, max = 20, message = "Buyer mnemonic must be between 1 and 20 characters.")
+  @NotNull(message = "This field is required.")
+  @Size(min = 1, max = 20, message = "Debited party id must be between 1 and 20 characters long.")
+  @Pattern(regexp = FieldValidationRegex.ONLY_NUMERIC_VALUES, message = "Only numeric values are allowed.")
   @DataField(pos = 7, name = "BuyerId")
-  private String buyerId;
+  @CsvBindByPosition(position = 6)
+  private String buyer;
 
+  @NotNull(message = "This field is required.")
   @Size(min = 1, max = 34, message = "Invoice number must be between 1 and 34 characters.")
   @DataField(pos = 8, name = "InvoiceNumber")
+  @CsvBindByPosition(position = 7)
   private String invoiceNumber;
 
-  @Pattern(regexp = DATE_FORMAT_REGEX, message = "Issue date must be in format dd-MM-yyyy.")
+  @NotNull(message = "This field is required.")
+  @Pattern(regexp = FieldValidationRegex.FORMATTED_DATE, message = "Issue date must be in format dd-MM-yyyy.")
   @DataField(pos = 9, name = "IssueDate", pattern = DATE_FORMAT)
-  private LocalDate issueDate;
+  @CsvBindByPosition(position = 8)
+  private String issueDate;
 
+  @NotNull(message = "This field is required.")
+  @Pattern(regexp = FieldValidationRegex.NUMBER_WITH_DECIMALS, message = "This field must be in format ##.##")
   @DataField(pos = 10, name = "FaceValueAmount", pattern = NUMBER_FORMAT, precision = 2)
-  private BigDecimal faceValueAmount;
+  @CsvBindByPosition(position = 9)
+  private String faceValueAmount;
 
+  @NotNull(message = "This field is required.")
   @Size(min = 1, max = 3, message = "Face value currency code must be between 1 and 3 characters.")
   @DataField(pos = 11, name = "FaceValueCurrency")
+  @CsvBindByPosition(position = 10)
   private String faceValueCurrency;
 
+  @NotNull(message = "This field is required.")
   @DataField(pos = 12, name = "OutstandingAmount", pattern = NUMBER_FORMAT, precision = 2)
-  private BigDecimal outstandingAmount;
+  @Pattern(regexp = FieldValidationRegex.NUMBER_WITH_DECIMALS, message = "This field must be in format ##.##")
+  @CsvBindByPosition(position = 11)
+  private String outstandingAmount;
 
+  @NotNull(message = "This field is required.")
   @Size(min = 1, max = 3, message = "Outstanding currency code must be between 1 and 3 characters.")
   @DataField(pos = 13, name = "OutstandingCurrency")
+  @CsvBindByPosition(position = 12)
   private String outstandingCurrency;
 
-  @Pattern(regexp = DATE_FORMAT_REGEX, message = "Settlement date must be in format dd-MM-yyyy.")
+  @NotNull(message = "This field is required.")
+  @Pattern(regexp = FieldValidationRegex.FORMATTED_DATE, message = "Settlement date must be in format dd-MM-yyyy.")
   @DataField(pos = 14, name = "SettlementDate", pattern = DATE_FORMAT)
-  private LocalDate settlementDate;
+  @CsvBindByPosition(position = 13)
+  private String settlementDate;
 
+  @NotNull(message = "This field is required.")
+  @Size(min = 1, max = 10, message = "This field must be between 1 and 10 character(s).")
+  @Pattern(regexp = FieldValidationRegex.ONLY_NUMERIC_VALUES, message = "Only numeric values are allowed.")
   @DataField(pos = 15, name = "AnchorCurrentAccount")
+  @CsvBindByPosition(position = 14)
   private String anchorCurrentAccount;
 }
