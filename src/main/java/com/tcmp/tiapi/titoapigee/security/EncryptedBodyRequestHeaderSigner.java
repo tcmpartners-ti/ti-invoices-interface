552be405aa6843f20726c370b788ce6efbd8d278
David Reyes <david.reyes@tcmpartners.com>
2023-10-26 15:30:07 -0500

    refactor(api-gee): make returned headers map mutable to extend it

--- src/main/java/com/tcmp/tiapi/titoapigee/security/EncryptedBodyRequestHeaderSigner.java
+++ src/main/java/com/tcmp/tiapi/titoapigee/security/EncryptedBodyRequestHeaderSigner.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
@@ -38,15 +39,16 @@ public class EncryptedBodyRequestHeaderSigner extends ApiGeeHeaderSigner impleme
       throw new UnrecoverableApiGeeRequestException(e.getMessage());
     }
 
-    return Map.ofEntries(
-      Map.entry(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE),
-      Map.entry("X-Api-Credentials", credentialsHeader),
-      Map.entry("X-Signature", signatureHeader),
-      Map.entry("X-Session", session),
-      Map.entry("X-Request-Id", requestId),
-      Map.entry("X-Guid", guid),
-      Map.entry("X-Device", device),
-      Map.entry("X-Device-Ip", deviceIp)
-    );
+    Map<String, String> headers = new HashMap<>();
+    headers.put(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+    headers.put("X-Api-Credentials", credentialsHeader);
+    headers.put("X-Signature", signatureHeader);
+    headers.put("X-Session", session);
+    headers.put("X-Request-Id", requestId);
+    headers.put("X-Guid", guid);
+    headers.put("X-Device", device);
+    headers.put("X-Device-Ip", deviceIp);
+
+    return headers;
   }
 }
