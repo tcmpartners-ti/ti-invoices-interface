15a70b66f42e2db09e07c95b3d5fb7b46a44188e
David Reyes <david.reyes@tcmpartners.com>
2023-10-26 15:31:20 -0500

    refactor(api-gee): update configurations keys

--- src/main/java/com/tcmp/tiapi/titoapigee/businessbanking/BusinessBankingConfiguration.java
+++ src/main/java/com/tcmp/tiapi/titoapigee/businessbanking/BusinessBankingConfiguration.java
@@ -5,14 +5,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 
 public class BusinessBankingConfiguration {
-  @Value("${bp.api-gee.services.business-banking.api-key}") private String businessBankingApiKey;
-  @Value("${bp.api-gee.services.business-banking.api-secret}") private String businessBankingApiSecret;
+  @Value("${bp.api-gee.credentials.api-key}") private String apiKey;
+  @Value("${bp.api-gee.credentials.api-secret}") private String apiSecret;
 
   @Bean
   public ApiGeeBodyEncryptionInterceptor operationalGatewaySigner() {
     return new ApiGeeBodyEncryptionInterceptor(
-      businessBankingApiKey,
-      businessBankingApiSecret
+      apiKey,
+      apiSecret
     );
   }
 }
