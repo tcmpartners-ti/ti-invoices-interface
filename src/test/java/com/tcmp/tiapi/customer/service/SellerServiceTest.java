c5204044ce5ba9ac8b5eed85118a70bb9088072f
David Reyes <david.reyes@tcmpartners.com>
2023-10-26 15:33:49 -0500

    refactor(test): remove unused imports

--- src/test/java/com/tcmp/tiapi/customer/service/SellerServiceTest.java
+++ src/test/java/com/tcmp/tiapi/customer/service/SellerServiceTest.java
@@ -11,12 +11,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
