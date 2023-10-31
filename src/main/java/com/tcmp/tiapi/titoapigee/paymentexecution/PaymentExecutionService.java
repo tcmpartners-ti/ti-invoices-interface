1d6329e119f3b928cd20610d24ad496f767b5158
David Reyes <david.reyes@tcmpartners.com>
2023-10-31 16:49:21 -0500

    refactor(payment-execution): made service more generic

--- src/main/java/com/tcmp/tiapi/titoapigee/paymentexecution/PaymentExecutionService.java
+++ src/main/java/com/tcmp/tiapi/titoapigee/paymentexecution/PaymentExecutionService.java
@@ -4,7 +4,9 @@ import com.tcmp.tiapi.titoapigee.dto.request.ApiGeeBaseRequest;
 import com.tcmp.tiapi.titoapigee.paymentexecution.dto.request.TransactionRequest;
 import com.tcmp.tiapi.titoapigee.paymentexecution.dto.request.TransactionType;
 import com.tcmp.tiapi.titoapigee.paymentexecution.dto.response.BusinessAccountTransfersResponse;
+import com.tcmp.tiapi.titoapigee.paymentexecution.exception.PaymentExecutionException;
 import com.tcmp.tiapi.titoapigee.security.HeaderSigner;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -19,96 +21,29 @@ public class PaymentExecutionService {
   private final HeaderSigner plainBodyRequestHeaderSigner;
   private final PaymentExecutionClient paymentExecutionClient;
 
-  public void processInvoiceWithPrePayment(
-    String bglAccount,
-    String anchorAccount,
-    String sellerAccount,
-    String invoiceReference,
-    BigDecimal amount
-  ) {
-    BusinessAccountTransfersResponse anchorToBgl = transferInvoiceTotalFromAnchorToBgl(anchorAccount, bglAccount, invoiceReference, amount);
-    BusinessAccountTransfersResponse bglToSeller = transferInvoiceTotalFromBglToSeller(bglAccount, sellerAccount, invoiceReference, amount);
-    var invoiceInterest = amount.multiply(BigDecimal.valueOf(0.1)); // Temporal
-    BusinessAccountTransfersResponse sellerToBgl = transferInvoiceInterestFromSellerToBgl(sellerAccount, bglAccount, invoiceReference, invoiceInterest);
-
-    log.info("Transferred $ {} (Interest $ {}) from {} (anchor) to {} (seller).", amount, invoiceInterest, anchorAccount, sellerAccount);
-    log.info("[Anchor - BGL] {}", anchorToBgl.data().creationDateTime());
-    log.info("[BGL - Seller] {}", bglToSeller.data().creationDateTime());
-    log.info("[Seller - BGL] {}", sellerToBgl.data().creationDateTime());
-  }
-
-  public void processInvoiceWithoutPrePayment(
-    String bglAccount,
-    String anchorAccount,
-    String sellerAccount,
-    String invoiceReference,
-    BigDecimal amount
-  ) {
-    BusinessAccountTransfersResponse anchorToBgl = transferInvoiceTotalFromAnchorToBgl(anchorAccount, bglAccount, invoiceReference, amount);
-    BusinessAccountTransfersResponse bglToSeller = transferInvoiceTotalFromBglToSeller(bglAccount, sellerAccount, invoiceReference, amount);
-
-    log.info("Transferred $ {} from {} (anchor) to {} (seller).", amount, anchorAccount, sellerAccount);
-    log.info("[Anchor - BGL] {}", anchorToBgl.data().creationDateTime());
-    log.info("[BGL - Seller] {}", bglToSeller.data().creationDateTime());
-  }
-
-  private BusinessAccountTransfersResponse transferInvoiceTotalFromAnchorToBgl(
-    String anchorAccount,
-    String bglAccount,
-    String invoiceReference,
+  public BusinessAccountTransfersResponse executeTransaction(
+    TransactionType transactionType,
+    String debtorAccount,
+    String creditorAccount,
+    String concept,
     BigDecimal amount
   ) {
     TransactionRequest requestData = TransactionRequest.from(
-      TransactionType.CLIENT_TO_BGL,
-      anchorAccount,
-      bglAccount,
-      "Descuento de factura %s".formatted(invoiceReference),
-      amount.toString()
+      transactionType,
+      debtorAccount,
+      creditorAccount,
+      concept,
+      amount
     );
 
     ApiGeeBaseRequest<TransactionRequest> request = new ApiGeeBaseRequest<>(requestData);
     Map<String, String> headers = plainBodyRequestHeaderSigner.buildRequestHeaders(request);
 
-    return paymentExecutionClient.postPayment(headers, request);
-  }
-
-  private BusinessAccountTransfersResponse transferInvoiceTotalFromBglToSeller(
-    String bglAccount,
-    String sellerAccount,
-    String invoiceReference,
-    BigDecimal amount
-  ) {
-    TransactionRequest requestData = TransactionRequest.from(
-      TransactionType.BGL_TO_CLIENT,
-      bglAccount,
-      sellerAccount,
-      "Descuento de factura %s".formatted(invoiceReference),
-      amount.toString()
-    );
-
-    ApiGeeBaseRequest<TransactionRequest> request = new ApiGeeBaseRequest<>(requestData);
-    Map<String, String> headers = plainBodyRequestHeaderSigner.buildRequestHeaders(request);
-
-    return paymentExecutionClient.postPayment(headers, request);
-  }
-
-  private BusinessAccountTransfersResponse transferInvoiceInterestFromSellerToBgl(
-    String sellerAccount,
-    String bglAccount,
-    String invoiceReference,
-    BigDecimal amount
-  ) {
-    TransactionRequest requestData = TransactionRequest.from(
-      TransactionType.BGL_TO_CLIENT,
-      bglAccount,
-      sellerAccount,
-      "Tasa descuento factura %s".formatted(invoiceReference),
-      amount.toString()
-    );
-
-    ApiGeeBaseRequest<TransactionRequest> request = new ApiGeeBaseRequest<>(requestData);
-    Map<String, String> headers = plainBodyRequestHeaderSigner.buildRequestHeaders(request);
-
-    return paymentExecutionClient.postPayment(headers, request);
+    try {
+      return paymentExecutionClient.postPayment(headers, request);
+    } catch (FeignException e) {
+      log.error("Could not execute transaction. {}", e.getMessage());
+      throw new PaymentExecutionException("Could not execute transaction.");
+    }
   }
 }
---
+++ src/main/java/com/tcmp/tiapi/titoapigee/paymentexecution/exception/PaymentExecutionException.java
@@ -0,0 +1,7 @@
+package com.tcmp.tiapi.titoapigee.paymentexecution.exception;
+
+public class PaymentExecutionException extends RuntimeException {
+  public PaymentExecutionException(String message) {
+    super(message);
+  }
+}
