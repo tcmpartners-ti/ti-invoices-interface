c4a6085e57757e282b55b47f8a6be489a4d35d21
David Reyes <david.reyes@tcmpartners.com>
2023-10-31 16:33:38 -0500

    feat(invoice): implement encoded account parser

---
+++ src/main/java/com/tcmp/tiapi/invoice/util/EncodedAccountParser.java
@@ -0,0 +1,31 @@
+package com.tcmp.tiapi.invoice.util;
+
+import lombok.Getter;
+
+public class EncodedAccountParser {
+  private static final int EXPECTED_ACCOUNT_NUMBER_LENGTH = 12;
+
+  private final String encodedAccount;
+
+  @Getter private String type;
+  @Getter private String account;
+
+  /**
+   * Builds the account object with the account number stored in a custom field in TI (EXTEVENT).
+   *
+   * @param encodedAccount The anchor account that has the format {@code ^(AH|CC)\d{10}$}.
+   */
+  public EncodedAccountParser(String encodedAccount) {
+    if (encodedAccount == null || encodedAccount.length() != EXPECTED_ACCOUNT_NUMBER_LENGTH) {
+      throw new IllegalArgumentException("Invalid anchor account.");
+    }
+
+    this.encodedAccount = encodedAccount;
+    parseAccount();
+  }
+
+  private void parseAccount() {
+    type = encodedAccount.substring(0, 1);
+    account = encodedAccount.substring(2);
+  }
+}
