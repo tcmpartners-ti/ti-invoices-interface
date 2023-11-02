dcc95e9aa48549acf182464a7a3707c7cc455da2
David Reyes <david.reyes@tcmpartners.com>
2023-11-02 11:45:24 -0500

    refactor(invoice): implement seatch product master ext by master id

--- src/main/java/com/tcmp/tiapi/invoice/repository/ProductMasterExtensionRepository.java
+++ src/main/java/com/tcmp/tiapi/invoice/repository/ProductMasterExtensionRepository.java
@@ -3,14 +3,11 @@ package com.tcmp.tiapi.invoice.repository;
 
 import com.tcmp.tiapi.invoice.model.ProductMasterExtension;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface ProductMasterExtensionRepository extends JpaRepository<ProductMasterExtension, Long> {
-  @Query("SELECT e.financeAccount FROM ProductMasterExtension e " +
-         "WHERE e.masterId = (SELECT m.id FROM ProductMaster m WHERE m.masterReference = :masterReference)")
-  Optional<String> findFinanceAccountByMasterReference(String masterReference);
+  Optional<ProductMasterExtension> findByMasterId(Long masterId);
 }
