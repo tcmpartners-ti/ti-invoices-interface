fb8a60a5e426dead0378e504711c4c87826f529f
David Reyes <david.reyes@tcmpartners.com>
2023-11-02 11:44:25 -0500

    refactor(invoice): add finance ack message builder

--- src/main/java/com/tcmp/tiapi/invoice/dto/ti/financeack/FinanceAckMessage.java
+++ src/main/java/com/tcmp/tiapi/invoice/dto/ti/financeack/FinanceAckMessage.java
@@ -7,12 +7,14 @@ import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlRootElement;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @XmlRootElement(name = "tfinvfindet", namespace = TINamespace.CONTROL)
