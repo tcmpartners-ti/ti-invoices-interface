40ae8f3f1a18b64d4781428439dc46551829c371
David Reyes <david.reyes@tcmpartners.com>
2023-11-06 12:32:37 -0500

    feat(messaging): add FinanceAckMessage in AckServiceRequest

--- src/main/java/com/tcmp/tiapi/messaging/model/requests/AckServiceRequest.java
+++ src/main/java/com/tcmp/tiapi/messaging/model/requests/AckServiceRequest.java
@@ -1,6 +1,7 @@
 package com.tcmp.tiapi.messaging.model.requests;
 
 import com.tcmp.tiapi.invoice.dto.ti.CreateDueInvoiceEventMessage;
+import com.tcmp.tiapi.invoice.dto.ti.financeack.FinanceAckMessage;
 import com.tcmp.tiapi.messaging.model.TINamespace;
 import com.tcmp.tiapi.messaging.model.TIOperation;
 
@@ -10,27 +11,35 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@XmlRootElement(name = "ServiceRequest",namespace = TINamespace.CONTROL)
+@XmlRootElement(name = "ServiceRequest", namespace = TINamespace.CONTROL)
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlSeeAlso({
-  CreateDueInvoiceEventMessage.class
+  CreateDueInvoiceEventMessage.class,
+  FinanceAckMessage.class
 })
 
 public class AckServiceRequest<T> {
 
-  @XmlElement(name = "RequestHeader",namespace = TINamespace.CONTROL)
+  @XmlElement(name = "RequestHeader", namespace = TINamespace.CONTROL)
   private AckRequestHeader header;
 
-  @XmlElement(
-      name = TIOperation.DUE_INVOICE_VALUE,
+  @XmlElements({
+    @XmlElement(
+      name = TIOperation.DUE_INVOICE_DETAILS_VALUE,
       type = CreateDueInvoiceEventMessage.class,
       namespace = TINamespace.CONTROL
-  )
-
+    ),
+    @XmlElement(
+      name = TIOperation.FINANCE_ACK_INVOICE_DETAILS_VALUE,
+      type = FinanceAckMessage.class,
+      namespace = TINamespace.CONTROL
+    ),
+  })
   private T body;
 
 }
