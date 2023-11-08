f44883a05ee7088c327dfe2190dcdaed88468b4b
David Reyes <david.reyes@tcmpartners.com>
2023-11-08 09:21:52 -0500

    refactor(invoice): add builder to dtos

--- src/main/java/com/tcmp/tiapi/invoice/dto/ti/financeack/Invoice.java
+++ src/main/java/com/tcmp/tiapi/invoice/dto/ti/financeack/Invoice.java
@@ -6,10 +6,12 @@ import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlRootElement;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @XmlRootElement(name = "InvoiceArray", namespace = TINamespace.CONTROL)
