fb5c9ca3255c59250e2a881a06a9488dc796f130
TCMP <franklin.rodriguez@tcmpartners.com>
2023-11-09 16:42:04 -0500

    feat(api-gee): determine the public IP address

--- src/main/java/com/tcmp/tiapi/titoapigee/config/ApiGeeConfiguration.java
+++ src/main/java/com/tcmp/tiapi/titoapigee/config/ApiGeeConfiguration.java
@@ -4,14 +4,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tcmp.tiapi.titoapigee.security.EncryptedBodyRequestHeaderSigner;
 import com.tcmp.tiapi.titoapigee.security.PlainBodyRequestHeaderSigner;
 import com.tcmp.tiapi.titoapigee.security.HeaderSigner;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.InetAddress;
+
 @Configuration
+@Slf4j
 public class ApiGeeConfiguration {
-  private static final String DEVICE_IP = "10.0.0.1";
+  private static final String DEFAULT_DEVICE_IP = "10.0.0.1";
   @Value("${bp.api-gee.headers.device}") private String device;
 
   @Value("${bp.api-gee.credentials.app-id}") private String appId;
@@ -21,12 +25,23 @@ public class ApiGeeConfiguration {
   @Bean
   @Qualifier("encryptedBodyRequestHeaderSigner")
   public HeaderSigner encryptedBodyRequestHeaderSigner(ObjectMapper objectMapper) {
-    return new EncryptedBodyRequestHeaderSigner(objectMapper, appId, apiKey, apiSecret, device, DEVICE_IP);
+    return new EncryptedBodyRequestHeaderSigner(objectMapper, appId, apiKey, apiSecret, device, getPublicIp());
   }
 
   @Bean
   @Qualifier("plainBodyRequestHeaderSigner")
   public HeaderSigner plainBodyRequestHeaderSigner(ObjectMapper objectMapper) {
-    return new PlainBodyRequestHeaderSigner(objectMapper, appId, apiKey, apiSecret, device, DEVICE_IP);
+    return new PlainBodyRequestHeaderSigner(objectMapper, appId, apiKey, apiSecret, device, getPublicIp());
   }
+
+  private String getPublicIp(){
+    try {
+      InetAddress direccionIP = InetAddress.getLocalHost();
+      return direccionIP.getHostAddress();
+    } catch (java.net.UnknownHostException e) {
+       log.info("Could not determine the public IP address.");
+       return DEFAULT_DEVICE_IP;
+    }
+  }
+
 }
