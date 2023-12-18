5ec66f92e2941e24c100ea4d0298ef7785c932b9
Daniel Alexander Arias Enríquez <daniel.arias@tcmpartners.com>
2023-12-18 11:18:21 -0500

    feat(test): Cancellation Flow Strategy Unit Test

---
+++ src/test/java/com/tcmp/tiapi/invoice/strategy/ticc/InvoiceCancellationFlowStrategyTest.java
@@ -0,0 +1,86 @@
+package com.tcmp.tiapi.invoice.strategy.ticc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+import com.tcmp.tiapi.customer.model.Address;
+import com.tcmp.tiapi.customer.model.Customer;
+import com.tcmp.tiapi.customer.repository.CustomerRepository;
+import com.tcmp.tiapi.invoice.dto.ti.cancel.CancelInvoiceEventMessage;
+import com.tcmp.tiapi.invoice.model.InvoiceMaster;
+import com.tcmp.tiapi.invoice.repository.InvoiceRepository;
+import com.tcmp.tiapi.ti.dto.request.AckServiceRequest;
+import com.tcmp.tiapi.titoapigee.operationalgateway.OperationalGatewayService;
+import com.tcmp.tiapi.titoapigee.operationalgateway.model.InvoiceEmailInfo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+class InvoiceCancellationFlowStrategyTest {
+  @Mock private OperationalGatewayService operationalGatewayService;
+
+  @Mock private InvoiceRepository invoiceRepository;
+  @Mock private CustomerRepository customerRepository;
+
+  @Captor private ArgumentCaptor<InvoiceEmailInfo> invoiceEmailInfoArgumentCaptor;
+
+  private InvoiceCancellationFlowStrategy invoiceCancellationFlowStrategy;
+
+  @BeforeEach
+  void setUp() {
+    invoiceCancellationFlowStrategy =
+        new InvoiceCancellationFlowStrategy(
+            operationalGatewayService, invoiceRepository, customerRepository);
+  }
+
+  @Test
+  void handleServiceRequest_itShouldNotifyInvoiceCancellation() {
+    var message = buildMockMessage();
+
+    when(customerRepository.findFirstByIdMnemonic(anyString()))
+        .thenReturn(
+            Optional.of(
+                Customer.builder()
+                    .address(Address.builder().customerEmail("jjoestar@dann.com").build())
+                    .fullName("Joseph Joestar       ")
+                    .build()));
+    when(invoiceRepository.findByProductMasterMasterReference(anyString()))
+        .thenReturn(
+            Optional.of(
+                InvoiceMaster.builder()
+                    .faceValueAmount(BigDecimal.valueOf(1010))
+                    .faceValueCurrencyCode("USD              ")
+                    .build()));
+
+    invoiceCancellationFlowStrategy.handleServiceRequest(new AckServiceRequest<>(null, message));
+
+    verify(customerRepository).findFirstByIdMnemonic(anyString());
+    verify(invoiceRepository).findByProductMasterMasterReference(anyString());
+    verify(operationalGatewayService)
+        .sendNotificationRequest(invoiceEmailInfoArgumentCaptor.capture());
+
+    assertEquals("1722466421", invoiceEmailInfoArgumentCaptor.getValue().customerMnemonic());
+    assertEquals("jjoestar@dann.com", invoiceEmailInfoArgumentCaptor.getValue().customerEmail());
+    assertEquals("Joseph Joestar", invoiceEmailInfoArgumentCaptor.getValue().customerName());
+    assertEquals(new BigDecimal("10.10"), invoiceEmailInfoArgumentCaptor.getValue().amount());
+    assertEquals("USD", invoiceEmailInfoArgumentCaptor.getValue().invoiceCurrency());
+  }
+
+  private CancelInvoiceEventMessage buildMockMessage() {
+    return CancelInvoiceEventMessage.builder()
+        .sellerIdentifier("1722466421")
+        .cancellationDate("2023-12-18")
+        .invoiceNumber("001-002-000000782")
+        .masterRef("12345")
+        .build();
+  }
+}
