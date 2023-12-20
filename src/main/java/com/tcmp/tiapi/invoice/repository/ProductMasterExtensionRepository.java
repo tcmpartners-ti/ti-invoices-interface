7ebd3e388e2483c9d75e517bef497cb087c9ac49
David Reyes <david.reyes@tcmpartners.com>
2023-12-20 17:24:53 -0500

    refactor(invoice): simplify query ro find product master by master reference

--- src/main/java/com/tcmp/tiapi/invoice/repository/ProductMasterExtensionRepository.java
+++ src/main/java/com/tcmp/tiapi/invoice/repository/ProductMasterExtensionRepository.java
@@ -10,18 +10,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProductMasterExtensionRepository
     extends JpaRepository<ProductMasterExtension, Long> {
-  Optional<ProductMasterExtension> findByMasterId(Long masterId);
-
   @Query(
       """
       SELECT e
         FROM ProductMasterExtension e
-        WHERE e.masterId = (
-          SELECT p.id
-          FROM ProductMaster p
-          WHERE
-            p.masterReference = :masterReference
-      )
+        LEFT JOIN ProductMaster p ON p.id = e.masterId
+        WHERE p.masterReference = :masterReference
     """)
   Optional<ProductMasterExtension> findByMasterReference(
       @Param("masterReference") String masterReference);
