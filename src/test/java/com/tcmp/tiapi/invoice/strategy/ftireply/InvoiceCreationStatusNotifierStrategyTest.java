dc7e0f2079744e6f2b6c77fd15fb57def2e619c0
David Reyes <david.reyes@tcmpartners.com>
2023-12-21 10:17:40 -0500

    refactor(invoice): update invoice creation notifier tests

--- src/test/java/com/tcmp/tiapi/invoice/strategy/ftireply/InvoiceCreationStatusNotifierStrategyTest.java
+++ src/test/java/com/tcmp/tiapi/invoice/strategy/ftireply/InvoiceCreationStatusNotifierStrategyTest.java
@@ -1,9 +1,9 @@
 package com.tcmp.tiapi.invoice.strategy.ftireply;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import com.tcmp.tiapi.invoice.model.InvoiceEventInfo;
 import com.tcmp.tiapi.invoice.service.InvoiceEventService;
@@ -13,9 +13,12 @@ import com.tcmp.tiapi.ti.dto.response.ServiceResponse;
 import com.tcmp.tiapi.titoapigee.businessbanking.BusinessBankingMapper;
 import com.tcmp.tiapi.titoapigee.businessbanking.BusinessBankingService;
 import com.tcmp.tiapi.titoapigee.businessbanking.dto.request.OperationalGatewayRequestPayload;
-import org.junit.jupiter.api.BeforeEach;
+import com.tcmp.tiapi.titoapigee.businessbanking.dto.request.PayloadStatus;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -25,52 +28,46 @@ class InvoiceCreationStatusNotifierStrategyTest {
   @Mock private BusinessBankingService businessBankingService;
   @Mock private BusinessBankingMapper businessBankingMapper;
 
-  private InvoiceCreationStatusNotifierStrategy invoiceCreationStatusNotifierStrategy;
+  @Captor private ArgumentCaptor<OperationalGatewayRequestPayload> payloadArgumentCaptor;
 
-  @BeforeEach
-  void setUp() {
-    invoiceCreationStatusNotifierStrategy =
-        new InvoiceCreationStatusNotifierStrategy(
-            invoiceEventService, businessBankingService, businessBankingMapper);
-  }
+  @InjectMocks private InvoiceCreationStatusNotifierStrategy invoiceCreationStatusNotifierStrategy;
 
   @Test
   void handleServiceResponse_itShouldDeleteInvoiceIfCreatedSuccessfully() {
-    var serviceResponse =
-        ServiceResponse.builder()
-            .responseHeader(
-                ResponseHeader.builder()
-                    .status(ResponseStatus.SUCCESS.getValue())
-                    .correlationId("123")
-                    .build())
+    var header =
+        ResponseHeader.builder()
+            .status(ResponseStatus.SUCCESS.getValue())
+            .correlationId("123")
             .build();
+    var serviceResponse = ServiceResponse.builder().responseHeader(header).build();
 
     invoiceCreationStatusNotifierStrategy.handleServiceResponse(serviceResponse);
 
     verify(invoiceEventService).deleteInvoiceByUuid("123");
+    verifyNoInteractions(businessBankingService);
   }
 
   @Test
   void handleServiceResponse_itShouldNotifyIfCreationFailed() {
-    var serviceResponse =
-        ServiceResponse.builder()
-            .responseHeader(
-                ResponseHeader.builder()
-                    .status(ResponseStatus.FAILED.getValue())
-                    .correlationId("123")
-                    .build())
+    var header =
+        ResponseHeader.builder()
+            .status(ResponseStatus.FAILED.getValue())
+            .correlationId("123")
             .build();
+    var serviceResponse = ServiceResponse.builder().responseHeader(header).build();
 
     when(invoiceEventService.findInvoiceEventInfoByUuid(anyString()))
         .thenReturn(InvoiceEventInfo.builder().build());
     when(businessBankingMapper.mapToRequestPayload(any(), any()))
-        .thenReturn(OperationalGatewayRequestPayload.builder().build());
+        .thenReturn(OperationalGatewayRequestPayload.builder().status("FAILED").build());
 
     invoiceCreationStatusNotifierStrategy.handleServiceResponse(serviceResponse);
 
     verify(invoiceEventService).findInvoiceEventInfoByUuid("123");
     verify(businessBankingMapper).mapToRequestPayload(any(), any());
-
     verify(invoiceEventService).deleteInvoiceByUuid("123");
+    verify(businessBankingService).notifyEvent(any(), payloadArgumentCaptor.capture());
+
+    assertEquals(PayloadStatus.FAILED.getValue(), payloadArgumentCaptor.getValue().status());
   }
 }
