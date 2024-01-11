a1f9b82489211ab1efd45a6967003c95535cc0f9
David Reyes <david.reyes@tcmpartners.com>
2024-01-11 15:55:21 -0500

    refactor(test): add test case to business banking service test

--- src/test/java/com/tcmp/tiapi/titoapigee/businessbanking/BusinessBankingServiceTest.java
+++ src/test/java/com/tcmp/tiapi/titoapigee/businessbanking/BusinessBankingServiceTest.java
@@ -1,21 +1,40 @@
 package com.tcmp.tiapi.titoapigee.businessbanking;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import com.tcmp.tiapi.shared.UUIDGenerator;
+import com.tcmp.tiapi.titoapigee.businessbanking.model.OperationalGatewayProcessCode;
 import com.tcmp.tiapi.titoapigee.security.HeaderSigner;
-import org.junit.jupiter.api.BeforeEach;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class BusinessBankingServiceTest {
-  @Mock private HeaderSigner businessBankingHeaderSigner;
+  @Mock private HeaderSigner encryptedBodyRequestHeaderSigner;
   @Mock private BusinessBankingClient businessBankingClient;
+  @Mock private UUIDGenerator uuidGenerator;
 
-  private BusinessBankingService testedService;
+  @InjectMocks private BusinessBankingService businessBankingService;
 
-  @BeforeEach
-  void setUp() {
-    testedService = new BusinessBankingService(businessBankingHeaderSigner, businessBankingClient);
+  @Test
+  void notifyEvent_itShouldSendRequest() {
+    var requestUuid = "000-0001";
+
+    when(encryptedBodyRequestHeaderSigner.buildRequestHeaders(any()))
+        .thenReturn(Map.of("Headers", "Yes!"));
+    when(uuidGenerator.getNewId()).thenReturn(requestUuid);
+
+    businessBankingService.notifyEvent(
+        OperationalGatewayProcessCode.INVOICE_CREATED,
+        Map.of("message", "This is a test payload."));
+
+    verify(businessBankingClient).notifyEvent(anyMap(), any());
   }
 }
