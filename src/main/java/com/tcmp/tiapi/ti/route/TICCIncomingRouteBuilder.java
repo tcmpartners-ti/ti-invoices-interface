53b6a8f7880ab7c204549753707fb3ae2488d18c
David Reyes <david.reyes@tcmpartners.com>
2024-01-24 17:34:11 -0500

    refactor(ti): decrease thread spawning for incoming routes

--- src/main/java/com/tcmp/tiapi/ti/route/TICCIncomingRouteBuilder.java
+++ src/main/java/com/tcmp/tiapi/ti/route/TICCIncomingRouteBuilder.java
@@ -21,7 +21,7 @@ public class TICCIncomingRouteBuilder extends RouteBuilder {
   public void configure() {
     from(uriFrom)
         .routeId("ticcIncomingQueue")
-        .threads(5, 10)
+        .threads(3, 6)
         .unmarshal(jaxbDataFormatAckEventRequest)
         .process()
         .body(AckServiceRequest.class, this::handleServiceRequest)
