5efa0a3bbb43330fe5fd322a975bb331cd257e85
David Reyes <david.reyes@tcmpartners.com>
2024-01-25 14:46:08 -0500

    chore: add startup listener

---
+++ src/main/java/com/tcmp/tiapi/StartupListener.java
@@ -0,0 +1,21 @@
+package com.tcmp.tiapi;
+
+import jakarta.annotation.Nonnull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+/** This class is used for deploy verification. */
+@Component
+@Slf4j
+public class StartupListener implements ApplicationListener<ApplicationReadyEvent> {
+  @Value("${BITBUCKET_COMMIT:unknown}")
+  private String commitHash;
+
+  @Override
+  public void onApplicationEvent(@Nonnull ApplicationReadyEvent event) {
+    log.info("Application built with commit [{}].", commitHash);
+  }
+}
