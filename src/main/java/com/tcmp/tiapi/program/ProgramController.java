2a2705247d7ea06f8d0f607fd88e94504c309cce
David Reyes <david.reyes@tcmpartners.com>
2024-02-05 16:33:14 -0500

    fix(customer/program): add security for path params

--- src/main/java/com/tcmp/tiapi/program/ProgramController.java
+++ src/main/java/com/tcmp/tiapi/program/ProgramController.java
@@ -2,6 +2,7 @@ package com.tcmp.tiapi.program;
 
 import com.tcmp.tiapi.customer.dto.request.CounterPartyDTO;
 import com.tcmp.tiapi.program.dto.response.ProgramDTO;
+import com.tcmp.tiapi.shared.FieldValidationRegex;
 import com.tcmp.tiapi.shared.dto.request.PageParams;
 import com.tcmp.tiapi.shared.dto.response.paginated.PaginatedResult;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,6 +11,9 @@ import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -34,7 +38,15 @@ public class ProgramController {
       schema = @Schema(type = "string"),
       in = ParameterIn.PATH,
       example = "ASEGURADORASUR")
-  public ProgramDTO getProgramById(@PathVariable String programId) {
+  public ProgramDTO getProgramById(
+      @PathVariable
+          @Valid
+          @NotNull(message = "This field is required.")
+          @Size(max = 35, message = "This field must have up to 35 characters")
+          @Pattern(
+              regexp = FieldValidationRegex.AVOID_SPECIAL_CHARACTERS,
+              message = "Special characters are not allowed")
+          String programId) {
     return programService.getProgramById(programId);
   }
 
@@ -59,7 +71,15 @@ public class ProgramController {
       in = ParameterIn.QUERY,
       example = "10")
   public PaginatedResult<CounterPartyDTO> getProgramSellersById(
-      @PathVariable String programId, @Parameter(hidden = true) @Valid PageParams pageParams) {
+      @PathVariable
+          @Valid
+          @NotNull(message = "This field is required.")
+          @Size(max = 35, message = "This field must have up to 35 characters")
+          @Pattern(
+              regexp = FieldValidationRegex.AVOID_SPECIAL_CHARACTERS,
+              message = "Special characters are not allowed")
+          String programId,
+      @Parameter(hidden = true) @Valid PageParams pageParams) {
     return programService.getProgramSellersById(programId, pageParams);
   }
 }
