f6fd605c4ecee78bab262a4a92cca9f8a0bcad1e
David Reyes <david.reyes@tcmpartners.com>
2024-02-07 17:39:00 -0500

    feat(invoice/seller): add filter for financed and not financed outstanding invoices

--- src/main/java/com/tcmp/tiapi/invoice/repository/InvoiceSpecifications.java
+++ src/main/java/com/tcmp/tiapi/invoice/repository/InvoiceSpecifications.java
@@ -14,16 +14,32 @@ import org.springframework.data.jpa.domain.Specification;
 public class InvoiceSpecifications {
   public static Specification<InvoiceMaster> filterBySellerMnemonicAndStatus(
       @Nonnull String sellerMnemonic, @Nullable String status) {
-    return (root, query, criteriaBuilder) -> {
+    return (root, query, cb) -> {
       List<Predicate> predicates = new ArrayList<>();
 
-      predicates.add(criteriaBuilder.equal(root.get("seller").get("mnemonic"), sellerMnemonic));
+      predicates.add(cb.equal(root.get("seller").get("mnemonic"), sellerMnemonic));
 
       if (status != null) {
-        predicates.add(criteriaBuilder.equal(root.get("status"), status));
+        // F status is not a native status, it's overridden with O status.
+        String dbStatus = status.equals("F") ? "O" : status;
+        predicates.add(cb.equal(root.get("status"), dbStatus));
+
+        Predicate invoiceHasBeenFinanced =
+            cb.and(
+                cb.not(root.get("isDrawDownEligible")),
+                cb.isNotNull(root.get("createFinanceEventId")),
+                cb.and(
+                    cb.isNotNull(root.get("discountDealAmount")),
+                    cb.notEqual(root.get("discountDealAmount"), 0)));
+
+        if (status.equals("O")) {
+          predicates.add(cb.not(invoiceHasBeenFinanced));
+        } else if (status.equals("F")) {
+          predicates.add(invoiceHasBeenFinanced);
+        }
       }
 
-      return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+      return cb.and(predicates.toArray(new Predicate[0]));
     };
   }
 }
