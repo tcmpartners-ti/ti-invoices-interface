8f0f03531d52101a6fff9cc79989eaf2ce722bb0
David Reyes <david.reyes@tcmpartners.com>
2024-03-18 15:03:01 -0500

    feat(customer): add dtos for bulk customer and account creation in ti

---
+++ src/main/java/com/tcmp/tiapi/customer/dto/csv/CustomerCreationCSVRow.java
@@ -0,0 +1,91 @@
+package com.tcmp.tiapi.customer.dto.csv;
+
+import com.opencsv.bean.CsvBindByName;
+import com.tcmp.tiapi.shared.FieldValidationRegex;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.camel.dataformat.bindy.annotation.CsvRecord;
+
+@CsvRecord(separator = ",")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerCreationCSVRow implements Serializable {
+  private static final String NUMBER_FORMAT = "#.##";
+  private static final String DATE_FORMAT = "dd-MM-yyyy";
+
+  @CsvBindByName(column = "SourceBankingBusiness", required = true)
+  @Size(min = 1, max = 8, message = "SourceBankingBusiness must be between 1 and 8 characters")
+  private String sourceBankingBusiness;
+
+  @CsvBindByName(column = "Branch", required = true)
+  @Size(min = 1, max = 8, message = "Branch must be between 1 and 8 characters")
+  private String branch;
+
+  @CsvBindByName(column = "Mnemonic", required = true)
+  @Size(min = 10, max = 13, message = "Mnemonic must be between 10 and 13 characters")
+  @Pattern(regexp = FieldValidationRegex.ONLY_NUMERIC_VALUES, message = "Mnemonic must be numeric")
+  private String mnemonic;
+
+  @CsvBindByName(column = "BankCode", required = true)
+  @Pattern(regexp = "0001|0003", message = "BankCode must be 0001 or 0003")
+  private String bankCode;
+
+  @CsvBindByName(column = "Number", required = true)
+  @Size(min = 3, max = 12, message = "Number must be between 3 and 12 characters")
+  private String number;
+
+  @CsvBindByName(column = "Type", required = true)
+  @Size(min = 3, max = 3, message = "Type must be 3 characters long")
+  private String type; // Commercial Destination in GAF
+
+  @CsvBindByName(column = "FullName", required = true)
+  @Size(min = 1, max = 35, message = "FullName must be between 1 and 35 characters")
+  private String fullName;
+
+  @CsvBindByName(column = "ShortName", required = true)
+  @Size(min = 1, max = 15, message = "ShortName must be between 1 and 15 characters")
+  private String shortName;
+
+  @CsvBindByName(column = "Address", required = true)
+  @Size(max = 1024, message = "Address must be less than 1024 characters")
+  private String address;
+
+  @CsvBindByName(column = "Phone", required = true)
+  @Size(min = 10, max = 20, message = "Phone must be between 10 and 20 characters")
+  private String phone;
+
+  @CsvBindByName(column = "Email", required = true)
+  @Size(min = 3, max = 128, message = "Email must be between 3 and 128 characters")
+  @Email
+  private String email;
+
+  @CsvBindByName(column = "AccountType", required = true)
+  @Size(min = 1, max = 10, message = "AccountType must be between 1 and 10 characters")
+  private String accountType;
+
+  @CsvBindByName(column = "AccountNumber", required = true)
+  @Size(min = 1, max = 34, message = "AccountNumber must be between 1 and 34 characters")
+  @Pattern(
+      regexp = FieldValidationRegex.BP_BANK_ACCOUNT,
+      message = "AccountNumber must begin with AH or CC followed by 10 digits")
+  private String accountNumber;
+
+  @CsvBindByName(column = "AccountCurrency", required = true)
+  @Size(min = 3, max = 3, message = "AccountCurrency must be 3 characters long")
+  @Pattern(regexp = "^[A-Z]{3}$", message = "AccountCurrency must be 3 uppercase letters")
+  private String accountCurrency;
+
+  @CsvBindByName(column = "AccountDateOpened", required = true)
+  @Pattern(
+      regexp = FieldValidationRegex.FORMATTED_DATE,
+      message = "Date must be in the format " + DATE_FORMAT)
+  private String accountDateOpened;
+}
