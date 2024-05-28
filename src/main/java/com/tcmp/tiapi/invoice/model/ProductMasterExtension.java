8be2b57afcf5cf2a589ce2431a95c4239ff3b660
David Reyes <david.reyes@tcmpartners.com>
2024-05-28 16:24:58 -0500

    chore(invoice): add gaf fields to product master extension [skip ci]

--- src/main/java/com/tcmp/tiapi/invoice/model/ProductMasterExtension.java
+++ src/main/java/com/tcmp/tiapi/invoice/model/ProductMasterExtension.java
@@ -1,6 +1,7 @@
 package com.tcmp.tiapi.invoice.model;
 
 import jakarta.persistence.*;
+import java.math.BigDecimal;
 import lombok.*;
 
 @Entity
@@ -21,4 +22,31 @@ public class ProductMasterExtension {
 
   @Column(name = "MASTER", nullable = false)
   private Long masterId;
+
+  @Column(name = "GAFOPEID", nullable = false)
+  private String gafOperationId;
+
+  @Column(name = "GAFINTRT", nullable = false)
+  private String gafInterestRate;
+
+  @Column(name = "GAFDIAMT", nullable = false)
+  private BigDecimal gafDisbursementAmount;
+
+  @Column(name = "GAFFACTO", nullable = false)
+  private BigDecimal gafFactor;
+
+  @Column(name = "BGAFINTS", nullable = false)
+  private BigDecimal buyerInterestsAmount;
+
+  @Column(name = "SGAFINTS", nullable = false)
+  private BigDecimal sellerInterestsAmount;
+
+  @Column(name = "BSOLCAMT", nullable = false)
+  private BigDecimal buyerSolcaAmount;
+
+  @Column(name = "SSOLCAMT", nullable = false)
+  private BigDecimal sellerSolcaAmount;
+
+  @Column(name = "GAFAMORT", nullable = false)
+  private String amortizations;
 }
